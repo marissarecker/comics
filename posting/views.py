c0bed119f73e49410055c43cb83d76338f4a72c3
Alex Recker <arecker@healthgrades.com>
2015-10-06 21:16:12 -0500

    Adds handling for an empty DB

--- posting/views.py
+++ posting/views.py
@@ -8,7 +8,13 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['latest'] = models.ComicStrip.objects.latest()
+
+        try:
+            latest = models.ComicStrip.objects.latest()
+        except models.ComicStrip.DoesNotExist:
+            latest = None
+
+        context['latest'] = latest
         return context
 
 
